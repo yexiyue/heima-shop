89ec56c56de020c73a5ac58f4f4908e083ac5c00
yeshifu <yexiyue666@qq.com>
2022-06-15 15:56:17 +0800

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+import path from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni()],
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'src')
+    }
+  }
+});
